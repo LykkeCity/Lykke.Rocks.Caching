322b219ba4176ace90298c51a0924beef85cabf2
mihail.yartsev <mihail.yartsev@gmail.com>
2017-08-08 15:21:14 +0300

    Rolled back removal of GetWithLockExtensions

---
+++ src/Rocks.Caching.Tests/GetWithLock/GetAsyncTests.cs
@@ -0,0 +1,454 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rocks.Caching.Tests.Stubs;
+using TimeoutAttribute = Microsoft.VisualStudio.TestTools.UnitTesting.TimeoutAttribute;
+
+// ReSharper disable PossibleMultipleEnumeration
+// ReSharper disable ImplicitlyCapturedClosure
+// ReSharper disable UnusedMember.Global
+
+namespace Rocks.Caching.Tests.GetWithLock
+{
+    [TestClass]
+    public class GetAsyncTests
+    {
+        #region Public methods
+
+        [TestInitialize]
+        public void TestInitialize ()
+        {
+            GetWithLockExtensions.Locks.Clear ();
+        }
+
+
+        [TestCleanup]
+        public void TestCleanup ()
+        {
+            GetWithLockExtensions.Locks.Clear ();
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public async Task SingleThread_InvokesGetValueFunctionOnce ()
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+            var exec_count = 0;
+
+
+            // act
+            var actual = await cache.GetAsync
+                                   (cache_key,
+                                    () => Task.Run (() =>
+                                                    {
+                                                        exec_count++;
+                                                        return new CachableResult<string> ("aaa", CachingParameters.FromMinutes (1));
+                                                    }));
+
+
+            // assert
+            actual.Should ().Be ("aaa");
+
+            cache.Values
+                 .Should ().HaveCount (1)
+                 .And.ContainKey (cache_key)
+                 .WhichValue.Value.Should ().Be ("aaa");
+
+            exec_count.Should ().Be (1);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public void TwoThreads_WithConcurency_InvokesGetValueFunctionOnce ()
+        {
+            MultiThreadWithConcurency_InvokesGetValueFunctionOnce_Test (2);
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public void FiveThreads_WithConcurency_InvokesGetValueFunctionOnce ()
+        {
+            MultiThreadWithConcurency_InvokesGetValueFunctionOnce_Test (5);
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public void TwoThreads_WithConcurency_GetResultThrows_BothThrows ()
+        {
+            MultiThreadWithConcurency_GetResultThrows_BothThrows_Test (2, sta: false);
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public void FiveThreads_WithConcurency_GetResultThrows_BothThrows ()
+        {
+            MultiThreadWithConcurency_GetResultThrows_BothThrows_Test (5, sta: false);
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public Task TwoTasks_WithConcurency_InvokesGetValueFunctionOnce ()
+        {
+            return MultiTaskWithConcurency_InvokesGetValueFunctionOnce_Test (2, sta: false);
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public Task FiveTasks_WithConcurency_InvokesGetValueFunctionOnce ()
+        {
+            return MultiTaskWithConcurency_InvokesGetValueFunctionOnce_Test (5, sta: false);
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public async Task ResultDataIsNull_CachesIt ()
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+            var exec_count = 0;
+
+            var create_result = new Func<Task<CachableResult<string>>>
+                (() => Task.Run
+                           (() =>
+                            {
+                                exec_count++;
+                                return new CachableResult<string> (null, CachingParameters.FromMinutes (1));
+                            }));
+
+
+            // act
+            var result = await cache.GetAsync (cache_key, create_result);
+            var result2 = await cache.GetAsync (cache_key, create_result);
+
+
+            // assert
+            cache.Values.Should ().HaveCount (1);
+            result.Should ().BeNull ();
+            result2.Should ().BeNull ();
+            exec_count.Should ().Be (1);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public async Task ResultDataIsNullAndDependencyKeysIncludeResult_DoesNotCache ()
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+            var exec_count = 0;
+
+            var create_result = new Func<Task<CachableResult<string>>>
+                (() => Task.Run (() =>
+                                 {
+                                     exec_count++;
+
+                                     return new CachableResult<string>
+                                         (null,
+                                          new CachingParameters (TimeSpan.FromMinutes (1)));
+                                 }));
+
+
+            // act
+            var result = await cache.GetAsync (cache_key, create_result);
+            var result2 = await cache.GetAsync (cache_key, create_result);
+
+
+            // assert
+            cache.Values.Should ().NotContainKey ("Test");
+            result.Should ().BeNull ();
+            result2.Should ().BeNull ();
+            exec_count.Should ().Be (2);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public async Task ResultIsNull_DoesNotCache ()
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+            var exec_count = 0;
+
+            var create_result = new Func<Task<CachableResult<string>>>
+                (() => Task.Run (() =>
+                                 {
+                                     exec_count++;
+                                     return (CachableResult<string>) null;
+                                 }));
+
+
+            // act
+            var result = await cache.GetAsync (cache_key, create_result);
+            var result2 = await cache.GetAsync (cache_key, create_result);
+
+
+            // assert
+            cache.Values.Should ().BeEmpty ();
+            result.Should ().BeNull ();
+            result2.Should ().BeNull ();
+            exec_count.Should ().Be (2);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public async Task ResultIsNull_ValueTypeResult_DoesNotCache ()
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+            var exec_count = 0;
+
+            var create_result = new Func<Task<CachableResult<int>>>
+                (() => Task.Run (() =>
+                                 {
+                                     exec_count++;
+                                     return (CachableResult<int>) null;
+                                 }));
+
+
+            // act
+            var result = await cache.GetAsync (cache_key, create_result);
+            var result2 = await cache.GetAsync (cache_key, create_result);
+
+
+            // assert
+            cache.Values.Should ().BeEmpty ();
+            result.Should ().Be (0);
+            result2.Should ().Be (0);
+            exec_count.Should ().Be (2);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        [TestMethod, Timeout (1000)]
+        public async Task NestedCalls_ReturnsItem ()
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+            var exec_count_a = 0;
+            var exec_count_b = 0;
+
+
+            // act
+            var result = await cache.GetAsync
+                                   (cache_key,
+                                    () => Task.Run
+                                              (async () =>
+                                                     {
+                                                         exec_count_a++;
+
+                                                         var res = await cache.GetAsync
+                                                                             ("NestedCalls_ReturnsItem",
+                                                                              () => Task.Run (() =>
+                                                                                              {
+                                                                                                  exec_count_b++;
+                                                                                                  return new CachableResult<string>
+                                                                                                      ("bbb",
+                                                                                                       CachingParameters.FromDays (1));
+                                                                                              }));
+
+                                                         return new CachableResult<string> (res, CachingParameters.FromDays (1));
+                                                     }));
+
+
+            // assert
+            result.Should ().Be ("bbb");
+            cache.Values.Should ().HaveCount (2);
+            exec_count_a.Should ().Be (1);
+            exec_count_b.Should ().Be (1);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static void MultiThreadWithConcurency_InvokesGetValueFunctionOnce_Test (int maxThreads)
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+
+            var exec_count = 0;
+            var start_event = new ManualResetEvent (false);
+
+            var results = new string[maxThreads];
+
+            var threads = Enumerable.Range (0, maxThreads).Select
+                (x =>
+                 {
+                     var thread = new Thread
+                         (() =>
+                          {
+                              start_event.WaitOne ();
+                              results[x] = cache.GetAsync
+                                  (cache_key,
+                                   () => Task.Run
+                                             (async () =>
+                                                    {
+                                                        await Task.Delay (TimeSpan.FromMilliseconds (100)).ConfigureAwait (false);
+
+                                                        Interlocked.Increment (ref exec_count);
+
+                                                        return new CachableResult<string> ("aaa", CachingParameters.FromMinutes (1));
+                                                    })).Result;
+                          });
+
+                     thread.IsBackground = true;
+
+                     return thread;
+                 })
+                                    .ToList ();
+
+
+            // act
+            threads.ForEach (t => t.Start ());
+            start_event.Set ();
+            threads.ForEach (t => t.Join ());
+
+
+            // assert
+            cache.Values
+                 .Should ().HaveCount (1)
+                 .And.ContainKey (cache_key)
+                 .WhichValue.Value.Should ().Be ("aaa");
+
+            results.Should ().OnlyContain (x => x == "aaa");
+            exec_count.Should ().Be (1);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        private static void MultiThreadWithConcurency_GetResultThrows_BothThrows_Test (int maxThreads, bool sta)
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+
+            var exec_count = 0;
+            var exception_count = 0;
+            var start_event = new ManualResetEvent (false);
+
+            var threads = Enumerable.Range (0, maxThreads).Select
+                (x =>
+                 {
+                     var thread = new Thread
+                         (() =>
+                          {
+                              start_event.WaitOne ();
+
+                              try
+                              {
+                                  cache.GetAsync<string> (cache_key,
+                                                          async () =>
+                                                                {
+                                                                    Interlocked.Increment (ref exec_count);
+
+                                                                    await Task.Delay (TimeSpan.FromMilliseconds (100)).ConfigureAwait (false);
+
+                                                                    throw new InvalidOperationException ();
+                                                                })
+                                       .Wait ();
+                              }
+                              catch (AggregateException)
+                              {
+                                  Interlocked.Increment (ref exception_count);
+                              }
+                          });
+
+                     thread.IsBackground = true;
+
+                     return thread;
+                 })
+                                    .ToList ();
+
+
+            // act
+            threads.ForEach (t => t.Start ());
+            start_event.Set ();
+            threads.ForEach (t => t.Join ());
+
+
+            // assert
+            exec_count.Should ().Be (1);
+            exception_count.Should ().Be (maxThreads);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+
+        private static async Task MultiTaskWithConcurency_InvokesGetValueFunctionOnce_Test (int maxTasks, bool sta)
+        {
+            // arrange
+            var cache = new CacheProviderStub ();
+            var cache_key = Guid.NewGuid ().ToString ();
+
+            var exec_count = 0;
+            var start_event = new SemaphoreSlim (0, maxTasks);
+
+            var results = new string[maxTasks];
+
+            var task_factory = Task.Factory;
+
+            var tasks = Enumerable.Range (0, maxTasks).Select
+                (x => task_factory.StartNew
+                          (async () =>
+                                 {
+                                     await start_event.WaitAsync ();
+
+                                     await Task.Delay (TimeSpan.FromMilliseconds (100));
+
+                                     Interlocked.Increment (ref exec_count);
+
+                                     var result = cache.GetAsync
+                                         (cache_key,
+                                          async () =>
+                                                {
+                                                    await Task.Delay (100).ConfigureAwait (false);
+
+                                                    return new CachableResult<string> ("aaa", CachingParameters.FromMinutes (10));
+                                                });
+
+                                     return result;
+                                 }
+                          ).Unwrap ()).ToList ();
+
+
+            // act
+            start_event.Release (maxTasks);
+            await Task.WhenAll (tasks);
+
+
+            // assert
+            cache.Values
+                 .Should ().HaveCount (1)
+                 .And.ContainKey (cache_key)
+                 .WhichValue.Value.Should ().Be ("aaa");
+
+            results.Should ().OnlyContain (x => x == "aaa");
+            exec_count.Should ().Be (1);
+
+            GetWithLockExtensions.Locks.Should ().BeEmpty ();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Rocks.Caching/GetWithLockExtensions.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Rocks.Caching
+{
+    /// <summary>
+    ///     A helpers class that can wrap operation of retrieving cached object value into thread safe lock
+    ///     to ensure that creation of cached value will be performed only once when needed.
+    /// </summary>
+    public static class GetWithLockExtensions
+    {
+        #region Private fields
+
+        private static readonly object EmptyResult = new object ();
+        internal static readonly ConcurrentDictionary<string, CachedResultLock> Locks = new ConcurrentDictionary<string, CachedResultLock> ();
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        ///     Gets cached object.
+        ///     Returns null if object was not found in cache.
+        /// </summary>
+        /// <typeparam name="T">Type of cached object.</typeparam>
+        /// <param name="cache">Cache provider. Can not be null.</param>
+        /// <param name="cachableObject">
+        ///     Instance of <see cref="ICachable&lt;T&gt;" /> that can provide data required
+        ///     for retrieval object value from <paramref name="cache" />. Can not be null.
+        /// </param>
+        [CanBeNull]
+        public static T Get<T> ([NotNull] this ICacheProvider cache, [NotNull] ICachable<T> cachableObject)
+        {
+            if (cachableObject == null)
+            {
+                throw new ArgumentNullException (nameof(cachableObject));
+            }
+
+            return cache.Get (cachableObject.GetCacheKey (), cachableObject.GetCachedValue);
+        }
+
+
+        /// <summary>
+        ///     Gets cached object by <paramref name="key" />.
+        ///     If object was not found in cache will call <paramref name="createResult" /> callback,
+        ///     store it's in the <paramref name="cache" /> and returns it's value.
+        /// </summary>
+        /// <typeparam name="T">Type of cached object.</typeparam>
+        /// <param name="cache">Cache provider. Can not be null.</param>
+        /// <param name="key">Cache key. Can not be null.</param>
+        /// <param name="createResult">
+        ///     A callback that should return data used in caching object
+        ///     if it was not found in <paramref name="cache" />. Can not be null.
+        /// </param>
+        [CanBeNull]
+        public static T Get<T> ([NotNull] this ICacheProvider cache, [NotNull] string key, [NotNull] Func<CachableResult<T>> createResult)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException (nameof(cache));
+            }
+
+            if (string.IsNullOrEmpty (key))
+            {
+                throw new ArgumentNullException (nameof(key));
+            }
+
+            if (createResult == null)
+            {
+                throw new ArgumentNullException (nameof(createResult));
+            }
+
+            // first - try the real cache
+            var res = cache.Get (key);
+            if (res == null)
+            {
+                // cache miss - obtain lock to execute createResult
+                // only once among possible concurrent requests
+                var cached_result_lock = AcquireCachedResultLock (key);
+
+                if (!cached_result_lock.IsExecuted)
+                {
+                    try
+                    {
+                        var result = createResult ();
+                        ProceedResult (cache, key, cached_result_lock, result);
+                    }
+                    catch (Exception ex)
+                    {
+                        ProceedExceptionResult (cached_result_lock, ex);
+                        throw;
+                    }
+                    finally
+                    {
+                        ReleaseCachedResultLock (key, cached_result_lock);
+                    }
+                }
+                else
+                {
+                    ReleaseCachedResultLock (key, cached_result_lock);
+                }
+
+                var exception_result = cached_result_lock.Result as ExceptionResult;
+                if (exception_result != null)
+                {
+                    throw exception_result.Exception;
+                }
+
+                res = cached_result_lock.Result;
+            }
+
+            if (res != EmptyResult && res != null)
+            {
+                return (T) res;
+            }
+
+            return default (T);
+        }
+
+
+        /// <summary>
+        ///     Gets cached object by <paramref name="key" />.
+        ///     If object was not found in cache will call <paramref name="createResult" /> callback,
+        ///     store it's in the <paramref name="cache" /> and returns it's value.
+        /// </summary>
+        /// <typeparam name="T">Type of cached object.</typeparam>
+        /// <param name="cache">Cache provider. Can not be null.</param>
+        /// <param name="key">Cache key. Can not be null.</param>
+        /// <param name="createResult">
+        ///     A callback that should return data used in caching object
+        ///     if it was not found in <paramref name="cache" />. Can not be null.
+        /// </param>
+        [ItemCanBeNull]
+        public static async Task<T> GetAsync<T> ([NotNull] this ICacheProvider cache,
+                                                 [NotNull] string key,
+                                                 [NotNull] Func<Task<CachableResult<T>>> createResult)
+        {
+            if (cache == null)
+            {
+                throw new ArgumentNullException (nameof(cache));
+            }
+
+            if (string.IsNullOrEmpty (key))
+            {
+                throw new ArgumentNullException (nameof(key));
+            }
+
+            if (createResult == null)
+            {
+                throw new ArgumentNullException (nameof(createResult));
+            }
+
+            // first - try the real cache
+            var res = cache.Get (key);
+            if (res == null)
+            {
+                // cache miss - obtain lock to execute createResult
+                // only once among possible concurrent requests
+                var cached_result_lock = AcquireCachedResultLock (key);
+
+                if (!cached_result_lock.IsExecuted)
+                {
+                    try
+                    {
+                        var result = await createResult ().ConfigureAwait (false);
+                        ProceedResult (cache, key, cached_result_lock, result);
+                    }
+                    catch (Exception ex)
+                    {
+                        ProceedExceptionResult (cached_result_lock, ex);
+                        throw;
+                    }
+                    finally
+                    {
+                        ReleaseCachedResultLock (key, cached_result_lock);
+                    }
+                }
+                else
+                {
+                    ReleaseCachedResultLock (key, cached_result_lock);
+                }
+
+                var exception_result = cached_result_lock.Result as ExceptionResult;
+                if (exception_result != null)
+                {
+                    throw exception_result.Exception;
+                }
+
+                res = cached_result_lock.Result;
+            }
+
+            if (res != EmptyResult && res != null)
+            {
+                return (T) res;
+            }
+
+            return default (T);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static CachedResultLock AcquireCachedResultLock (string key)
+        {
+            var result = Locks.GetOrAdd (key, x => new CachedResultLock ());
+
+            result.Mutex.Wait ();
+
+            return result;
+        }
+
+
+        private static void ReleaseCachedResultLock (string key, CachedResultLock cachedResultLock)
+        {
+            cachedResultLock.Mutex.Release ();
+
+            CachedResultLock tmp;
+            Locks.TryRemove (key, out tmp);
+        }
+
+
+        private static void ProceedResult<T> (ICacheProvider cache, string key, CachedResultLock cachedResultLock, [CanBeNull] CachableResult<T> cachableResult)
+        {
+            object result = null;
+
+            if (cachableResult != null)
+            {
+                // if cachableResult.Result is null, it's still a result. So we need to cache it as EmptyResult object
+                // so the next time we didn't get null from cache and had to go create result once again.
+                result = cachableResult.Result;
+                if (result == null)
+                {
+                    result = EmptyResult;
+                }
+
+                cache.Add (key, result, cachableResult.Parameters);
+            }
+
+            cachedResultLock.Result = result;
+            cachedResultLock.IsExecuted = true;
+        }
+
+
+        private static void ProceedExceptionResult (CachedResultLock cachedResultLock, Exception exception)
+        {
+            var result = new ExceptionResult { Exception = exception };
+
+            cachedResultLock.Result = result;
+            cachedResultLock.IsExecuted = true;
+        }
+
+        #endregion
+    }
+}
